c106125bd3129fcd3c3196a2ebeed1639aa6b370
hassibtleiji <hassibt@gmail.com>
2022-10-23 19:41:02 -0400

    3rd commit

---
+++ expressProject/public/precache-manifest.182afb727626598611b7d1bc2c92b42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dc0022fd30fdc5e388634d79f1bf35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff261c541767bf84799",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "aff261c541767bf84799",
+    "url": "/static/js/2.d192d6f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d192d6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469bffc485209878d63a",
+    "url": "/static/js/main.c08ba389.chunk.js"
+  },
+  {
+    "revision": "4ad3c1137b5909126752",
+    "url": "/static/js/runtime-main.d0317a8f.js"
+  }
+]);
